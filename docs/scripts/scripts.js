351f49130ec6ce7757cbb48ae800f29bc2ad5763
abeeeee <>
2019-06-04 12:23:03 +0900

    [ADD]docsの追加

---
+++ docs/scripts/scripts.js
@@ -0,0 +1,103 @@
+$("document").ready(function(){
+  $('.slick-box').slick({
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 900,
+    dots: true,
+    arrows: false,
+    centerMode: true,
+    centerPadding: '10%'
+  });
+});
+
+$("document").ready(function(){
+  $('.sp_slick-box').slick({
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 900,
+    dots: true,
+    arrows: false,
+    centerMode: true,
+    centerPadding: '10%'
+  });
+});
+
+$("document").ready(function(){
+  $('.work_slick-box').slick({
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 900,
+    dots: true,
+    arrows: false,
+    centerMode: true,
+    centerPadding: '10%'
+  });
+});
+
+$(function(){
+  var slider = "#slider";
+  var thumbnailItem = "#thumbnail-list .thumbnail-item";
+
+  $(thumbnailItem).each(function(){
+   var index = $(thumbnailItem).index(this);
+   $(this).attr("data-index",index);
+  });
+
+  $(slider).on('init',function(slick){
+   var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
+   $(thumbnailItem+'[data-index="'+index+'"]').addClass("thumbnail-current");
+  });
+
+  $(slider).slick({
+    autoplay: true,
+    arrows: false,
+    fade: true,
+    infinite: false
+  });
+
+  $(thumbnailItem).on('click',function(){
+    var index = $(this).attr("data-index");
+    $(slider).slick("slickGoTo",index,false);
+  });
+
+  $(slider).on('beforeChange',function(event,slick, currentSlide,nextSlide){
+    $(thumbnailItem).each(function(){
+      $(this).removeClass("thumbnail-current");
+    });
+    $(thumbnailItem+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
+  });
+});
+
+
+$(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    console.log(traX);
+    $(".title").css({"background-position": traX + "%" + traY + "%"});
+  });
+});
+
+$(function(){
+   $('a[href^="#"]').click(function() {
+      var speed = 800;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+});
+
+jQuery(document).ready(function(){
+  jQuery('.parallax-layer').parallax({
+      mouseport: jQuery("#port")
+  });
+});
+
