9c8dc67cad5f9d63e8e91ad0caef4e09af9566e2
abeeeee <>
2019-06-05 14:27:20 +0900

    不要なjsの消去

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -84,17 +84,6 @@ $(document).ready(function(){
   });
 });
 
-$(function(){
-   $('a[href^="#"]').click(function() {
-      var speed = 800;
-      var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-      return false;
-   });
-});
-
 jQuery(document).ready(function(){
   jQuery('.parallax-layer').parallax({
       mouseport: jQuery("#port")
