6cb97feeaf5e566078af83a36254240141fd5ac7
abeeeee <>
2019-06-07 12:43:16 +0900

    idの抜け、loading画面のレイヤー最上移動、一旦スムーススクロールコメントアウト

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -92,16 +92,16 @@ $(document).ready(function(){
   });
 });
 
-$(function(){
-   $('a[href^="#"]').click(function() {
-      var speed = 800;
-      var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-      return false;
-   });
-});
+// $(function(){
+//    $('a[href^="#"]').click(function() {
+//       var speed = 800;
+//       var href= $(this).attr("href");
+//       var target = $(href == "#" || href == "" ? 'html' : href);
+//       var position = target.offset().top;
+//       $('body,html').animate({scrollTop:position}, speed, 'swing');
+//       return false;
+//    });
+// });
 
 jQuery(document).ready(function(){
   jQuery('.parallax-layer').parallax({
