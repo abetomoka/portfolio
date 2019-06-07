03ebc3c22914d423e2b6e8eaf001a73c6fbf157f
abeeeee <>
2019-06-07 12:37:23 +0900

    docsを同期

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,3 +1,11 @@
+const anim = lottie.loadAnimation({
+  container: document.getElementById('bodymovin-wrapper'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+});
+
 $("document").ready(function(){
   $('.slick-box').slick({
     autoplay: true,
@@ -100,4 +108,18 @@ jQuery(document).ready(function(){
       mouseport: jQuery("#port")
   });
 });
+
+$(function() {
+  var h = $(window).height(); //ブラウザウィンドウの高さを取得
+  $('#main-contents').css('display','none'); //初期状態ではメインコンテンツを非表示
+  $('#loader-bg ,#loader').height(h).css('display','block'); //ウィンドウの高さに合わせでローディング画面を表示
+});
+$(window).load(function () {
+  $('#loader-bg').delay(900).fadeOut(800); //$('#loader-bg').fadeOut(800);でも可
+  $('#loader').delay(600).fadeOut(300); //$('#loader').fadeOut(300);でも可
+  $('#main-contents').css('display', 'block'); // ページ読み込みが終わったらメインコンテンツを表示する
+});
+
+
+
 
