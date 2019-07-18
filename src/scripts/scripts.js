e0111749768304bb4af1788d44b76489f5a1c266
abeeeee <>
2019-07-18 14:24:36 +0900

    [fix]スムーズスクロールの調整

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -132,3 +132,23 @@ $(window).load(function () {
   $('#main-contents').css('display', 'block'); // ページ読み込みが終わったらメインコンテンツを表示する
 });
 
+//スムーズスクロール
+jQuery(function(){
+  // ★　任意のズレ高さピクセル数を入力　↓
+  var headerHight = 200;
+   // #で始まるアンカーをクリックした場合に処理
+   jQuery('a[href^=#]').click(function() {
+    // スクロールの速度
+    var speed = 400; // ミリ秒
+    // アンカーの値取得
+    var href= jQuery(this).attr("href");
+    // 移動先を取得
+    var target = jQuery(href == "#" || href == "" ? 'html' : href);
+    // 移動先を数値で取得
+    var position = target.offset().top-headerHight; // ※　-headerHightでズレの処理
+    // スムーズスクロール
+    jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+   });
+});
+
